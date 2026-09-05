f989c639595a8dfc822164a01a24c8f74e24224a
PSN <psn396@gmail.com>
2026-09-05 13:36:41 +0530

    Initial Working Baseline - Release 0.0.3

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="QBMP",
-    version="0.0.2",
+    version="0.0.3",
     description="Declarative synthetic dataset generation through mathematical modelling",
     packages=["QBMP"],
     package_dir={"": "SRCS"},
